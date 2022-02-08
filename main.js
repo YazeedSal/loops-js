17bc249bbe9500a77c983b0555d9daac64258d4d
YazeedSalameh <yazeedsalameh02082005@gmail.com>
2022-02-08 21:01:52 +0200

    finished every thing

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+
+let messege = "bla blalbasjjnasdjkn*** ^^^ ,,, hdvhvff,"
+let specialCharachters = "*^,"
+let counter = 0
+for (let i = 0; messege[i]; i++) {
+
+    for (let j = 0; specialCharachters[j]; j++) {
+        if (messege[i]== specialCharachters[j]) {
+            counter++
+        }
+    }
+
+}
+console.log(counter);
